2dd916d16af6027217f77fc2eccbab6b94cc2e98
bluething <habib.gunadarma@gmail.com>
2021-08-27 17:18:10 +0700

    Another way to concat the flux

--- reactor-test/src/test/java/io/github/bluething/spring/reactor/OperatorTest.java
+++ reactor-test/src/test/java/io/github/bluething/spring/reactor/OperatorTest.java
@@ -40,4 +40,17 @@ public class OperatorTest {
 
         Thread.sleep(4000);
     }
+
+    @Test
+    void concatWith() throws InterruptedException {
+        Flux<Integer> oneToFive = Flux.range(1, 5)
+                .delayElements(Duration.ofMillis(200));
+        Flux<Integer> sixToTen = Flux.range(6, 5)
+                .delayElements(Duration.ofMillis(400));
+
+        oneToFive.concatWith(sixToTen)
+              .subscribe(System.out::println);
+
+        Thread.sleep(4000);
+    }
 }
