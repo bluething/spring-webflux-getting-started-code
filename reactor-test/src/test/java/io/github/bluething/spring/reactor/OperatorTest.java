0fcbcff912f51c8d6b1fb6d70ce49df13e86f393
bluething <habib.gunadarma@gmail.com>
2021-08-27 17:15:50 +0700

    Flux from mono

--- reactor-test/src/test/java/io/github/bluething/spring/reactor/OperatorTest.java
+++ reactor-test/src/test/java/io/github/bluething/spring/reactor/OperatorTest.java
@@ -2,6 +2,7 @@ package io.github.bluething.spring.reactor;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public class OperatorTest {
     @Test
@@ -15,6 +16,13 @@ public class OperatorTest {
     public void flatMap() {
         Flux.range(1, 5)
                 .flatMap(i -> Flux.range(i * 10, 2))
+                .subscribe(System.out::println);
+    }
+
+    @Test
+    public void flatMapMany() {
+        Mono.just(3)
+                .flatMapMany(i -> Flux.range(1, i))
                 .subscribe(System.out::println);
     }
 }
