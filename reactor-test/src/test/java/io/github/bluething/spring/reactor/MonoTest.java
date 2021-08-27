72e4cffce0796015cb73861eb0e418b5b1d949a7
bluething <habib.gunadarma@gmail.com>
2021-08-27 14:30:10 +0700

    Do something on subscriber

--- reactor-test/src/test/java/io/github/bluething/spring/reactor/MonoTest.java
+++ reactor-test/src/test/java/io/github/bluething/spring/reactor/MonoTest.java
@@ -12,4 +12,13 @@ public class MonoTest {
     public void monoWithConsumer() {
         Mono.just("A").log().subscribe(s -> System.out.println(s));
     }
+
+    public void monoWithDoOn() {
+        Mono.just("A")
+                .log()
+                .doOnSubscribe(subs -> System.out.println("Subscribed: " + subs))
+                .doOnRequest(request -> System.out.println("Requested: " + request))
+                .doOnSuccess(success -> System.out.println("Succeed: " + success))
+                .subscribe();
+    }
 }
